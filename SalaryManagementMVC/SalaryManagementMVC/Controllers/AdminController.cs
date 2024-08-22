e63ce977e0daae1be76450e5ca073c5ff9c0eb2a
Tija <tijarose94@gmail.com>
2024-08-22 23:12:39 +0530

    add Navigation Link to  all pages in AdminDashboard

--- SalaryManagementMVC/SalaryManagementMVC/Controllers/AdminController.cs
+++ SalaryManagementMVC/SalaryManagementMVC/Controllers/AdminController.cs
@@ -13,17 +13,50 @@ namespace SalaryManagementMVC.Controllers
         {
             return View("Home");
         }
+
         public ActionResult Register()
         {
             return View("Registration");
         }
+
         public ActionResult Dashboard()
         {
             return View("Dashboard");
         }
+
         public ActionResult CreateEmployee()
         {
             return View("CreateEmployee");
+        }
+
+        public ActionResult ManagePayroll()
+        {
+            return View("Payroll");
+        }
+
+        public ActionResult ManageLoan()
+        {
+            return View("Loan");
+        }
+
+        public ActionResult ManageLeave()
+        {
+            return View("Leave");
+        }
+
+        public ActionResult ManageDeduction()
+        {
+            return View("Deduction");
+        }
+
+        public ActionResult ManageDepartment()
+        {
+            return View("Department");
+        }
+
+        public ActionResult ManageAllowance()
+        {
+            return View("Allowance");
         }
     }
 }
